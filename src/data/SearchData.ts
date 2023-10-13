1de874fa2b4902e332af4a8201407bf1ed6db032
TedZy829 <tadeus.frederick@gmail.com>
2023-10-13 19:41:59 +0800

    RegisterDomain file created, extra components, add data folder, move component folder

---
+++ src/data/SearchData.ts
@@ -0,0 +1,18 @@
+export const words = [
+    'gav123.emn',
+    'gavcool.emn',
+    '0xgav.emn',
+    'imgav.emn',
+    'ted123.emn',
+    'tedcool.emn',
+    '0xted.emn',
+    'imted.emn',
+    'bjk123.emn',
+    'bjkcool.emn',
+    '0xbjk.emn',
+    'imbjk.emn',
+    'del123.emn',
+    'delcool.emn',
+    '0xdel.emn',
+    'imdel.emn',
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-import HeroLayout from './components/HeroLayout'
+import Navbar from '@/components/Navbar'
+import HeroLayout from '@/components/HeroLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
