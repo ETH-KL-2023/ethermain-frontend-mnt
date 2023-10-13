0225e55b7d9d4a98b33b91ab6167ca58af315a63
TedZy829 <tadeus.frederick@gmail.com>
2023-10-13 18:47:02 +0800

    home page

---
+++ src/pages/components/HeroLayout.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import React, { useState } from "react";
+import { Text } from "@chakra-ui/react";
+import router from "next/router";
+
+const HeroLayout = () => {
+
+  const handleRegisterRoute = () => {
+    router.push('/RegisterDomain');
+  }
+  const handleMarketplaceRoute = () => {
+    router.push('/Marketplace');
+  }
+  const handleProfileRoute = () => {
+    router.push('/Profile');
+  }
+
+  return (
+    <div className="h-screen flex-col mx-auto justify-center items-center place-content-center bg-gradient-to-r from-blue-100 via-pink-100 to-purple-100">
+      <Text
+        fontSize="7xl"
+        className="pt-32 text-center font-bold"
+        color="black"
+      >
+        The Web3 Domain Registrar
+      </Text>
+      <Text fontSize="xl" className="text-center font-bold mb-4 text-slate-500">
+        The right platform for you to host a domain name with ease.
+      </Text>
+      <div className="w-1/3 flex-col mx-auto items-center justify-center place-content-center">
+        <button className="w-full bg-transparent rounded-lg border-2 border-slate-500 p-4 text-lg font-semibold text-slate-500 my-4"
+        onClick={handleRegisterRoute}>
+          Register a New Domain
+        </button>
+        <button className="w-full bg-transparent rounded-lg border-2 border-slate-500 p-4 text-lg font-semibold text-slate-500 my-4"
+        onClick={handleMarketplaceRoute}>
+          Marketplace: Buy Domain
+        </button>
+        <button className="w-full bg-transparent rounded-lg border-2 border-slate-500 p-4 text-lg font-semibold text-slate-500 my-4"
+        onClick={handleProfileRoute}>
+          Profile: Manage Domains
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroLayout;
---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+import router, { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+
+const Navbar = () => {
+  const handleHomeRoute = () => {
+    router.push("/index");
+  };
+  const handleRegisterRoute = () => {
+    router.push("/RegisterDomain");
+  };
+  const handleMarketplaceRoute = () => {
+    router.push("/Marketplace");
+  };
+  const handleProfileRoute = () => {
+    router.push("/Profile");
+  };
+
+  return (
+    <div className="h-[70px] flex justify-between items-center bg-gradient-to-r from-blue-100 via-pink-100 to-purple-100">
+      <Link href={"/"} className="ml-[30px]">
+        {/* <Image src={logo} alt="" width={130} height={100} /> */}
+      </Link>
+      <div className="flex items-center gap-4 mr-[30px]">
+        <button
+          className="w-[100px] bg-green-main hover:bg-green-main text-black font-semibold py-2 px-4 rounded-full"
+          onClick={handleHomeRoute}
+        >
+          Home
+        </button>
+        <button
+          className="w-[180px] bg-green-main hover:bg-green-main text-black font-semibold py-2 px-4 rounded-full"
+          onClick={handleRegisterRoute}
+        >
+          Register a Domain
+        </button>
+
+        <button
+          className="w-[130px] bg-green-main hover:bg-green-main text-black font-semibold py-2 px-4 rounded-full"
+          onClick={handleMarketplaceRoute}
+        >
+          Marketplace
+        </button>
+        <button
+          className="w-[100px] bg-green-main hover:bg-green-main text-black font-semibold py-2 px-4 rounded-full"
+          onClick={handleProfileRoute}
+        >
+          Profile
+        </button>
+        <ConnectButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
