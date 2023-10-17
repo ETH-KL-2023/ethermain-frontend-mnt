06a3a0f26e3a3a8d802100256d0b5e65962a6c7e
0xGavinOwen <gavinowen713@gmail.com>
2023-10-17 17:06:17 +0800

    fix mentel 75

--- src/pages/ConfirmMarketplace/[dnsInput].tsx
+++ src/pages/ConfirmMarketplace/[dnsInput].tsx
@@ -176,7 +176,7 @@ function ConfirmMarketplace() {
     write?.();
   }
 
-  const [subtotalPrice, setSubtotalPrice] = useState(0.015);
+  const [subtotalPrice, setSubtotalPrice] = useState(75);
 
   return (
     <div className="h-screen bg-gradient-to-r from-blue-100 via-pink-100 to-purple-100">
